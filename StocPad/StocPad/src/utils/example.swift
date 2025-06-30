5a9c0d746fd232f39be960088cd02aaeda5c9d75
carlosamorim28 <81266635+carlosamorim28@users.noreply.github.com>
2025-06-30 16:33:07 -0300

    Add files via upload

---
+++ StocPad/StocPad/src/utils/example.swift
@@ -0,0 +1,7 @@
+//
+//  example.swift
+//  StocPad
+//
+//  Created by Carlos Jorge on 30/06/25.
+//
+
---
+++ StocPad/StocPad/src/view/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StocPad
+//
+//  Created by Carlos Jorge on 30/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
